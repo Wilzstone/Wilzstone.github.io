aa61cf4a2b244598918be6e469802ffc36026111
Wilston Laurentius Dai Keraf <wilstonkeraf301@gmail.com>
2023-07-16 13:22:54 +0700

    test tcuy

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Project Wilston</title>
+    <!-- Masukkan pustaka Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Menu Projek</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Beranda</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Project Vaksin</h5>
+        <p class="card-text">Tugas blocking 1</p>
+        <a href="pawil/login.php" class="btn btn-primary">GO</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Project Coffee</h5>
+        <p class="card-text">Tugas blocking 2.</p>
+        <a href="wilcafe/index.php" class="btn btn-primary">GO</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+    <!-- Masukkan pustaka Bootstrap JS dan jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
